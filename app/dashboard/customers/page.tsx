0ade0700f93f1ca10d7ed89001dbb7b60fa37539
khalidtazurkaev <barzaev97@gmail.com>
2024-10-06 10:05:42 +0200

    1

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            customers
+        </div>
+    );
+};
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            invoices
+        </div>
+    );
+};
+
+export default Page;
