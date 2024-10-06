7361d67f580094333d228bbf9a81b3d11ab7a5ba
Халид <78869405+barzaevhalid@users.noreply.github.com>
2024-10-06 10:19:37 +0200

    Update Nav-links.tsx
    commited on github

--- app/dashboard/Nav-links.tsx
+++ app/dashboard/Nav-links.tsx
@@ -6,6 +6,15 @@ import {
 import Link from 'next/link';
 
 // ...
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
 
 export default function NavLinks() {
     return (
